7234884747c923235bd0f7a7260756e894e81766
kanaujiyaadarsh986-pixel <kanaujiyaadarsh986@gmail.com>
2026-04-13 12:24:15 +0530

    new

--- Backend/db.js
+++ Backend/db.js
@@ -1,4 +1,4 @@
-import pg from "pg";
+const pg = require("pg");
 const { Pool } = pg;
 
 const pool = new Pool({
@@ -6,4 +6,4 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }
 });
 
-export default pool;
+module.exports = pool;
