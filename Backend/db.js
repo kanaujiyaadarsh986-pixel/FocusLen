157b2da0fdce1b8cdd4d8821ea7393878cbe0be3
kanaujiyaadarsh986-pixel <kanaujiyaadarsh986@gmail.com>
2026-04-13 12:13:13 +0530

    Change database connection string in
    Updated the connection string to use a specific PostgreSQL database.

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgresql://adarsh_kanaujia_user:VGXSAoKJkNSC9qQQDlqibEyns6WCNtaF@dpg-d7e8obt8nd3s73bab3n0-a.oregon-postgres.render.com/adarsh_kanaujia",
   ssl: { rejectUnauthorized: false }
 });
 
